e5cfbac1c27f889eb304d6cb94e0e9dc8d557197
Dev <dev.kumar@thinkingwithtools.com>
2018-05-24 17:40:57 -0400

    added card sections

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -10,13 +10,14 @@ class AlbumList extends Component{
         axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data }));
     }
 
-    renderAlbums(){
+    renderAlbums() {
         return this.state.albums.map(album => 
-        <AlbumDetail key={album.title}>{album.title} album={album} />
-    };
+        <AlbumDetail key={album.title} album={album} />
+        );
     }
 
     render(){
+        console.log(this.state);
         return (
             <View>
                 {this.renderAlbums()}
